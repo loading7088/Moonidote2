95ddce7715d2b0b4d9e366c272ade361974381ab
Jake Williams <example.email.ussername@gmail.com>
2023-07-17 12:19:33 -0600

    First actual commit

--- Assets/movePlayer.cs
+++ Assets/movePlayer.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class movePlayer : MonoBehaviour
 {
+    public GameObject Player;
+
+    public Rigidbody2D rb;
+
+    public float moveSpeed = 10f;
+    public int jumpstrength = 6;
+    public int deathZone = -15;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,28 @@ public class movePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Move left and right
+        if(Input.GetKey(KeyCode.A))
+        {
+            Player.transform.Translate(Vector3.left  * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        //JUMP!!!
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.AddForce(Vector2.up * jumpstrength, ForceMode2D.Impulse);
+        }
+
+        //Kills you if you fall to your death
+        if (Player.transform.position.x < deathZone)
+        {
+            Destroy(Player);
+            Debug.Log("Make game over screen");
+        }
     }
 }
