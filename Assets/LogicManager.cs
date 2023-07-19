041cff30d4bce7575242602370c68a1b9dc93824
Jake Williams <example.email.ussername@gmail.com>
2023-07-19 16:06:02 -0600

    Sprites again

--- Assets/LogicManager.cs
+++ Assets/LogicManager.cs
@@ -6,8 +6,15 @@ public class LogicManager : MonoBehaviour
 {
     public GameObject child;
     public GameObject adult;
+    public GameObject player;
     public GameObject gun;
     public GameObject bullet;
+
+    public SpriteRenderer gunRend;
+    public SpriteRenderer playerRend;
+    public Sprite gunFire;
+    public Sprite playerGun;
+
 
     public Vector3 bulletOffset = new Vector3(0.4f, 0, 0);
 
@@ -34,6 +41,8 @@ public class LogicManager : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(bullet, gun.transform.position + bulletOffset, bullet.transform.rotation);
+            gunRend.sprite = gunFire;
+            playerRend.sprite = playerGun;
         }         
     }
 }
